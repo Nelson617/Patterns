c41084c03229083992ffc67cbc1b09b55ecf080e
nelson <ralph_nelson25@milton.edu>
2023-04-10 14:13:43 -0400

    Initial commit

---
+++ src/PatternMaker.java
@@ -0,0 +1,46 @@
+public class PatternMaker {
+    public static void main(String[] args) {
+        PatternMaker stripes = new PatternMaker();
+    }
+
+    public PatternMaker(){
+        System.out.println("Hola Pattern Maker");
+
+        pattern1();
+        pattern2();
+        pattern3();
+        pattern5();
+    }
+
+    public void pattern1(){
+        for(int x = 0; x < 6; x++){
+       //     for(int y = 0; y < 5; y++){
+                System.out.println(x*5);
+       //     }
+        }
+    }
+
+    public void pattern2(){
+        for(int n = 0; n < 6; n++){
+            System.out.println(2+n*5);
+
+        }
+
+        }
+        public void pattern3(){
+        for (int w = 21; w > 0; w=w-4){
+            System.out.print(w*4 + "\t");
+        }
+        }
+
+
+        public void pattern5() {
+            System.out.println();
+            for (int z = 0; z < 4; z++) {
+                for (int c = 1; c < 5; c++) {
+                    System.out.print(c + "\t");
+                }
+                System.out.println();
+            }
+        }
+}
